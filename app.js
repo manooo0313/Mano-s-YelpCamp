e7cb5ee518ef6cfc58103b335012d1e69b99be8b
lothdavidmano <loth.david.mano@hotmail.com>
2018-11-22 12:25:51 +0000

    add mongodb connect to mlab

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://Mano0313:mano86@ds255455.mlab.com:55455/yelpcampmano");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
