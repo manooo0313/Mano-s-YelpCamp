6fb9dce4b548c84715811834cc18de3cc2a4f2c4
lothdavidmano <loth.david.mano@hotmail.com>
2018-11-22 13:14:27 +0000

    add database url

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
+console.log(process.env.DATABASEURL);
+
 //mongoose.connect("mongodb://localhost/yelp_camp_v12");
 mongoose.connect("mongodb://Mano0313:mano86@ds255455.mlab.com:55455/yelpcampmano");
 
